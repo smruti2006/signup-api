39d76652c2128deea5b22d08f2fdd1edce2c0aca
Smruti Ranjan Sahoo <risingsmruti@gmail.com>
2025-06-28 21:26:50 +0530

    Updated MongoDB connection with async/await

--- Model/db.js
+++ Model/db.js
@@ -1,14 +1,16 @@
-const mongoose = require("mongoose")
-//Connection
+const mongoose = require("mongoose");
+
+// Connection
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       "mongodb+srv://risingsmruti:A3HHPider2KKJpLl@cluster0.mongodb.net/signupapi?retryWrites=true&w=majority"
     );
-    console.log(`MongoDB Connected: {conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Stop the app if DB fails to connect
   }
-}
+};
+
 module.exports = connectDB;
