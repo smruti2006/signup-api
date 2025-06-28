dc0be6a937fc5d513412f119933b38d3d09c3663
Smruti Ranjan Sahoo <risingsmruti@gmail.com>
2025-06-28 21:16:24 +0530

    Updated MongoDB Atlas URI

--- Model/db.js
+++ Model/db.js
@@ -1,14 +1,14 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 //Connection
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/Authonticationsystem`, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://risingsmruti:A3HHPider2KKJpLl@cluster0.mongodb.net/signupapi?retryWrites=true&w=majority"
+    );
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
 }
-module.exports=connectDB;
+module.exports = connectDB;
